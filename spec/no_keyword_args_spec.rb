069feee057b365453b3e5f7f7270e75a20f9a97e
fkdkent <fkdkent@agileware.jp>
2018-04-24 21:14:22 +0900

    Add specs

---
+++ spec/no_keyword_args_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe RuboCop::Cop::Lint::NoKeywordArgs do
+  subject(:cop) { described_class.new }
+
+  describe 'Method definitions by def keyword' do
+    it {
+      expect_offense(<<~RUBY)
+        def foo(bar:); end
+                ^^^^ Do not use keyword arguments
+      RUBY
+    }
+
+    it { expect_no_offenses('def foo(bar); end') }
+  end
+
+  describe 'Method definitions by define_method' do
+    it {
+      expect_offense(<<~RUBY)
+        define_method :foo do |bar:|; end
+                               ^^^^ Do not use keyword arguments
+      RUBY
+    }
+
+    it { expect_no_offenses('define_method :foo do |bar|; end') }
+  end
+end
